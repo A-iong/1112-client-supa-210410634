b8c53b61ed7514b108404709ecfee1fb1ec6d56b
A-iong <210410634@gms.tku.edu.tw>
2023-05-25 01:35:06 +0800

    W15-P4: fetch menu data from local json

--- src/App_34.js
+++ src/App_34.js
@@ -5,6 +5,8 @@ import HomePage_34 from "./pages/HomePage_34";
 import MenuStaticPage_34 from "./pages/local/MenuStaticPage_34";
 import SupaMenuPage_34 from "./pages/supabase/MenuPagea_34";
 import SupaMenuByCategoryPage from "./pages/supabase/MenuByCategoryPage_34";
+import MenuNodePage_34 from "./pages/node/MenuNodePage_34";
+import MenuLocalJsonPage_34 from "./pages/local/MenuLocalJsonPage_34";
 
 const App_34 = () => {
   return (
@@ -17,6 +19,8 @@ const App_34 = () => {
           path="/supa_menu_34/:category"
           element={<SupaMenuByCategoryPage />}
         />
+        <Route path="/node_menu_34" element={<MenuNodePage_34 />} />
+        <Route path="/json_menu_34" element={<MenuLocalJsonPage_34 />} />
       </Routes>
     </BrowserRouter>
   );
